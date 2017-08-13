ca68b89c540a28b6284f50f02e4aa65fe14387a3
PharmyOf1 <phillip.harm@gmail.com>
2017-08-13 14:50:44 -0400

    some changes

--- d3_git/d3/urls.py
+++ d3_git/d3/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from django.contrib import admin
-from .views import home
+from .views import home, load_data
 
 urlpatterns = [
     url(r'^$', home, name='home'),
+    url(r'^api/load_data', load_data, name='load_data'),
 ]
--- d3_git/d3/views.py
+++ d3_git/d3/views.py
@@ -1,16 +1,26 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from .hive import QueryData, MDLZHive
+from django.http import JsonResponse
 import pandas as pd
 import json
 
 def home(request):
+    return render(request, "d3/index.html", {})
+
+def load_data(request):
     hive = MDLZHive.ConnectHive(database='sales_reporting')
-    q = hive.execute("""SELECT fiscal_day, sum(total_gross_revenue) as revenue from daily_store_sku_transactional_dsc where fiscal_day > '2017-07-07' group by fiscal_day limit 10
+    q = hive.execute("""SELECT fiscal_day, sum(total_gross_revenue) as revenue
+                        FROM daily_store_sku_transactional_dsc
+                        WHERE fiscal_day > '2017-01-01'
+                        GROUP BY fiscal_day
                        """)
+
+    # q.columns=['date','close']
+    # q = q.to_json()
 
     q = [tuple(x) for x in q.values]
     fields = ['date', 'close']
     dicts = [dict(zip(fields, d)) for d in q]
     data = {'data':dicts}
-    return render(request, "d3/index.html", data)
+    return JsonResponse(dicts,safe=False)
